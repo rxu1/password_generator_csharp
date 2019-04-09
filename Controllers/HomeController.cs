a5dc51600709e61bb0b52239304f8b2f527efa1e
Raymond Xu <raymondxu92@gmail.com>
2019-04-08 22:09:22 -0700

    first commit, added sessions and random string

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using PasswordGenerator.Models;
+
+namespace PasswordGenerator.Controllers
+{
+  public class HomeController : Controller
+  {
+    private static Random random = new Random();
+    public static string RandomString(int length)
+    {
+      const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      return new string(Enumerable.Repeat(characters, length).Select(s => s[random.Next(s.Length)]).ToArray()); 
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+      int? counter = HttpContext.Session.GetInt32("counter");
+      counter = (counter==null) ? 1: counter; 
+      counter++;
+
+      ViewBag.Count = counter; 
+      ViewBag.passcode = RandomString(14);
+      HttpContext.Session.SetInt32("counter", (int)counter);
+      // return View();
+
+      TempData["RandomString"] = RandomString(14);
+      ViewBag.String = TempData["RandomString"];
+      return View();
+      
+    }
+
+  }
+}
